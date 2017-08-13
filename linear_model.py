eee0163ce4ed3e788f62ec767d6c4ebdacc027cd
OwenZhu <398094982@qq.com>
2017-08-13 22:03:32 +1000

    upload new samples

--- linear_model.py
+++ linear_model.py
@@ -12,6 +12,8 @@ display_step = 50
 # training data
 train_X = numpy.asarray(
     [3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167, 7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+# add some noises to the training data
+train_X = train_X + rng.randn(train_X.shape[0])
 train_Y = numpy.asarray(
     [1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221, 2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
 n_samples = train_X.shape[0]
@@ -27,7 +29,7 @@ W = tf.Variable(rng.randn(), name="weight")
 b = tf.Variable(rng.randn(), name="bias")
 
 # Construct a linear model
-activation = tf.add(tf.mul(X, W), b)
+activation = tf.add(tf.multiply(X, W), b)
 
 # Minimize the squared errors
 cost = tf.reduce_sum(tf.pow(activation - Y, 2)) / (2 * n_samples)  # L2 loss
@@ -45,6 +47,20 @@ with tf.Session() as sess:
         for (x, y) in zip(train_X, train_Y):
             sess.run(optimizer, feed_dict={X: x, Y: y})
 
-    # Display logs per epoch step
-    if epoch % display_step == 0:
-        print()
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print(
+                "Epoch:", '%04d' % (epoch + 1), "cost=", \
+                "{:.9f}".format(sess.run(cost, feed_dict={X: train_X, Y: train_Y})), \
+                "W=", sess.run(W), "b=", sess.run(b))
+
+    print("Optimization Finished!")
+    print(
+        "cost=", sess.run(cost, feed_dict={X: train_X, Y: train_Y}), \
+        "W=", sess.run(W), "b=", sess.run(b))
+
+    # Graphic display
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
